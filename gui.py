546b9d273b5cfccfdff8387ab2cf0bee96cb8386
Nick Voelker <volkoff223@gmail.com>
2024-02-09 19:16:33 -0500

    GUI and logic connected and working

--- gui.py
+++ gui.py
@@ -1,68 +1,62 @@
-from tkinter import messagebox, ttk
-from tkinter import filedialog as fd 
-import tkinter as tk
-import os
+import customtkinter
 
-def display_selection():
-    # Get the selected value.
-    selection_year = input_year.get()
-    selection_month = input_month.get()
-    messagebox.showinfo(
-        message=f"The selected year is: {selection_year}\nThe selected month is: {selection_month}",
-        title="Selection"
-    )
-def open_file(): 
-  
-    # Specify the file types 
-    filetypes = [('Excel file', '*.xls')] 
-  
-    # Show the open file dialog by specifying path 
-    f = fd.askopenfile(filetypes=filetypes) 
-  
-    if f:
-      filepath = os.path.abspath(f.name)
-  
-
-main_window = tk.Tk()
-main_window.config(width=300, height=200)
-main_window.title("Jen's Quick Scan")
+from login import login_and_scan
 
 
-# user input for month
-label = ttk.Label(text="Please select a month:")
-label.place(x=50, y=35)
-input_month = ttk.Combobox(
-    state="readonly",
-    values=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"]
-)
-input_month.place(x=50, y=50)
+class DSSReportFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_columnconfigure(0, weight=1)
+        
+        def button_callback():
+            id = self.center_login_id.get()
+            password = self.center_password.get()
+            center = self.center_combobox.get()
+            login_and_scan(center, id, password)
+            print(center, id, password)
+            
+
+        self.label = customtkinter.CTkLabel(self, text="Choose a center", fg_color="transparent", width=300)
+        self.label.grid(row=0, column=0, pady=0)
+
+        center_combobox_var = customtkinter.StringVar(value="")
+        self.center_combobox = customtkinter.CTkComboBox(self, values=['AOTK', 'Agapy', 'All Day Daycare', 'Right Start'], variable=center_combobox_var, width=300)
+        self.center_combobox.set('')
+        self.center_combobox.grid(row=1, column=0, padx=10, pady=0)
+        center_combobox_var.set('')
+
+        self.label = customtkinter.CTkLabel(self, text="Login ID", fg_color="transparent", width=300)
+        self.label.grid(row=2, column=0, pady=(10,0))
+
+        self.center_login_id = customtkinter.CTkEntry(self, width=300)
+        self.center_login_id.grid(row=3, column=0, padx=10, pady=0)
+
+        self.label = customtkinter.CTkLabel(self, text="Password", fg_color="transparent", width=300)
+        self.label.grid(row=4, column=0, pady=(10,0))
+
+        self.center_password = customtkinter.CTkEntry(self, width=300)
+        self.center_password.grid(row=5, column=0, padx=10, pady=0)
 
 
-# user input for year
-label = ttk.Label(text="Please select a year:")
-label.place(x=50, y=85)
-input_year = ttk.Combobox(
-    state="readonly",
-    values=["2024", "2025", "2026"]
-)
-input_year.place(x=50, y=100)
+        self.button = customtkinter.CTkButton(self, text="Create DSS Report", command=button_callback, width=300)
+        self.button.grid(row=6, column=0, padx=10, pady=10)
 
-# Create an open file button 
-label = ttk.Label(text="Please select a file:")
-label.place(x=50, y=135)
-open_button = ttk.Button(main_window, text='Open a File', command=open_file) 
-open_button.place(x=50, y=150)
 
-# Display selections
-button = ttk.Button(text="Display selection", command=display_selection)
-button.place(x=50, y=200)
 
-# Create a textfield for putting the 
-# text extracted from file 
-text = tk.Text(main_window, height=12) 
-  
-# Specify the location of textfield 
-text.grid(column=0, row=0, sticky='nsew') 
-text.place(x=50, y=300)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-main_window.mainloop()
+        self.title("DSS Scan Tool")
+        self.geometry("600x400")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.dss_report_frame = DSSReportFrame(self)
+        self.dss_report_frame.grid(row=0, column=0, padx=10, pady=10)
+
+
+
+app = App()
+app.mainloop()
--- login.py
+++ login.py
@@ -2,33 +2,89 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
+import io
+from datetime import date
+import datetime
 
-username = "511002120"
-passwrd = "Bailey1028"
 
-options = webdriver.ChromeOptions()
-#options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.vaecc.org/eccpw")
-el_username = driver.find_element(By.NAME, "login")
-el_username.send_keys(username)
-el_password = driver.find_element(By.NAME, "password")
-el_password.send_keys(passwrd)
-el_login_btn = driver.find_element(By.CLASS_NAME, "login-btn")
-el_login_btn.click()
-cont_btn = driver.find_element(By.TAG_NAME, 'button')
-cont_btn.click()
-cont_btn = driver.find_element(By.NAME, 'contin')
-cont_btn.click()
-report_button = driver.find_element(By.XPATH, '//button[contains(text(),"ATTENDANCE REPORT")]')
-report_button.click()
-max_rows = driver.find_element(By.NAME, "maxRows")
-max_rows.click()
-option = driver.find_element(By.XPATH, '//option[contains(text(), "100")]')
-option.click()
 
-html = driver.page_source
-df = pd.read_html(html, match='provider_attendance')
-df[3].to_csv('text.csv')
+today = datetime.datetime.now()
+first_date_of_month = today.replace(day=1)
 
-driver.close()
+
+def login_and_scan(center, id, password):
+    # Selenium to login and get to correct page
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.vaecc.org/eccpw")
+    el_username = driver.find_element(By.NAME, "login")
+    el_username.send_keys(id)
+    el_password = driver.find_element(By.NAME, "password")
+    el_password.send_keys(password)
+    el_login_btn = driver.find_element(By.CLASS_NAME, "login-btn")
+    el_login_btn.click()
+    cont_btn = driver.find_element(By.TAG_NAME, 'button')
+    cont_btn.click()
+    cont_btn = driver.find_element(By.NAME, 'contin')
+    cont_btn.click()
+    report_button = driver.find_element(By.XPATH, '//button[contains(text(),"ATTENDANCE REPORT")]')
+    report_button.click()
+    max_rows = driver.find_element(By.NAME, "maxRows")
+    max_rows.click()
+    option = driver.find_element(By.XPATH, '//option[contains(text(), "100")]')
+    option.click()
+
+    # Grab table from html and remove all unusable data
+    pd.options.mode.copy_on_write = True
+    html = io.StringIO(driver.page_source)
+    data = pd.read_html(html, match='provider_attendance')
+    #grab correct table
+    df = data[3]
+    #remove usless data
+    df.drop([0,1,2,3,4], inplace=True)
+    #reset index
+    df.reset_index(drop=True, inplace=True)
+    #remove last nine lines
+    df=df[:-9]
+    #remove multi level column name
+    df.columns = df.columns.droplevel()
+    #remove usless columns
+    df.drop(columns=df.columns[1:4], inplace=True)
+    #remove dates in the future
+    df.drop(columns=df.columns[9:], inplace=True) ## change 9 to todays date
+
+    # rename all column names to date
+    index = 1
+    while index < len(df.columns):
+        df.rename(columns={str(index): first_date_of_month.replace(day=index).strftime('%a %d')}, inplace=True)
+        index +=1
+
+    # # remove weekend
+    for date in df.columns:
+        if date.startswith('S'):
+            df.drop([date], axis=1, inplace=True)
+
+    inconplete_scan = []
+    no_scan = []
+    for idate in df.columns:
+        for idx in df.index:
+            if df.loc[idx][idate] == 'I':
+                inconplete_scan_info = df.iloc[idx, 0] +" "+ idate + today.strftime('%B')
+                inconplete_scan.append(inconplete_scan_info)
+
+            elif type(df.loc[idx][idate]) == float:
+                no_scan_info = df.iloc[idx, 0] +" "+ idate + today.strftime('%B')
+                no_scan.append(no_scan_info)
+
+    print('INCOMPLETE SCANS:', inconplete_scan, 'NO SCANS:', no_scan)
+    file = open(center + '.txt', 'w')
+    file.write(center)
+    file.write("\nINCOMPLETE SCANS:\n")
+    for scan in inconplete_scan:
+        file.write(scan+'\n')
+    file.write("\nNO SCAN:\n")
+    for nscan in no_scan:
+        file.write(nscan+'\n')
+    file.close()
+    # driver.close()
